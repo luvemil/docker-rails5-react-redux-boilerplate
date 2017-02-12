c92bdbbedc06dc54a4adfc44c1ef52f08248c0b7
luvemil <taran.marco@gmail.com>
2017-02-12 19:18:15 +0100

    Generate digital_ocean_token hook on first run

--- scripts/generate_secrets.sh
+++ scripts/generate_secrets.sh
@@ -11,11 +11,14 @@ if [ ! -f ./config/secrets.yml ]; then
   cat > ./config/secrets.yml <<EOL
 development:
   secret_key_base: ${KEY_DEV}
+  digital_ocean_token: <%= ENV["DIGITALOCEAN_TOKEN"] %>
 
 test:
   secret_key_base: ${KEY_TEST}
+  digital_ocean_token: <%= ENV["DIGITALOCEAN_TOKEN"] %>
 
 production:
   secret_key_base: <%= ENV["SECRET_KEY_BASE"] %>
+  digital_ocean_token: <%= ENV["DIGITALOCEAN_TOKEN"] %>
 EOL
 fi
